1a999afe60e16ded7c71c9923c9e413a8b065187
Daniel Kasztan Słowik <kkasztann@gmail.com>
2018-05-31 02:58:57 +0200

    utworzono metode logout

--- Aplikacja/parkourSpots/src/app/services/auth.service.ts
+++ Aplikacja/parkourSpots/src/app/services/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    public afAuth: AngularFireAuth
+  ) { }
+
+
+
+
+
+
+
+
+  logout() {
+    return this.afAuth.auth.signOut();
+  }
+
+
+
 }
