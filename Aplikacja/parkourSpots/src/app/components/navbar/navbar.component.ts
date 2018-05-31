5cd12a6f81284c19bc854db6055e3c2ece79baa1
Daniel Kasztan Słowik <kkasztann@gmail.com>
2018-05-31 15:12:57 +0200

    wykorzystano getAuth w ngOnInit

--- Aplikacja/parkourSpots/src/app/components/navbar/navbar.component.ts
+++ Aplikacja/parkourSpots/src/app/components/navbar/navbar.component.ts
@@ -7,12 +7,24 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  public isLogin: boolean;
+  public name: string;
+  public email: string;
 
   constructor(
     public authService: AuthService
   ) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.isLogin = true;
+        this.name = auth.displayName;
+        this.email = auth.email;
+      } else {
+        this.isLogin = false;
+      }
+    })
   }
 
   onClickLogout() {
