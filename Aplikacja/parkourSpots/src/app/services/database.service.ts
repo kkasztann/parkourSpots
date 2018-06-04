41e2445feed781996fd051f3fa5f06f90007d1ad
Daniel Kasztan Słowik <kkasztann@gmail.com>
2018-06-04 17:27:24 +0200

    stworzono konsturktor dla DatabaseService

--- Aplikacja/parkourSpots/src/app/services/database.service.ts
+++ Aplikacja/parkourSpots/src/app/services/database.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { Spot } from '../models/spot';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
 
-  constructor(private afs: AngularFirestore) { }
+  spotsCol: AngularFirestoreCollection<Spot>;
+  spots: Observable<Spot[]>;
+
+  constructor(private afs: AngularFirestore) {
+    this.spotsCol = this.afs.collection('spots');
+    this.spots = this.spotsCol.valueChanges();
+   }
 }
