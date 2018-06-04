24d9fd57a115bcb60b2c03dcf951b89f714f5bd2
Daniel Kasztan Słowik <kkasztann@gmail.com>
2018-06-04 19:00:25 +0200

    dodano model SpotID

--- Aplikacja/parkourSpots/src/app/services/database.service.ts
+++ Aplikacja/parkourSpots/src/app/services/database.service.ts
@@ -10,11 +10,19 @@ import { Spot } from '../models/spot';
 export class DatabaseService {
 
   private spotsCol: AngularFirestoreCollection<Spot>;
-  public spots: Observable<Spot[]>;
+  public spots: any;
 
   constructor(private afs: AngularFirestore) {
     this.spotsCol = this.afs.collection('spots');
-    this.spots = this.spotsCol.valueChanges();
+    // this.spots = this.spotsCol.valueChanges();
+    this.spots = this.spotsCol.snapshotChanges()
+      .map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Spot;
+          const id = a.payload.doc.id;
+          return { id, data };
+        });
+      });
   }
 
   addSpot(newSpot: Spot) {
