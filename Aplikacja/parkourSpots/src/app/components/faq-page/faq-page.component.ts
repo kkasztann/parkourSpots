c6a4e2cd5c6697046b4e8d2ddc914653212a5b86
Daniel Kasztan Słowik <kkasztann@gmail.com>
2018-06-07 15:43:12 +0200

    dodano zmiane rozmiaru przycisku w przypadku mniejszych rozmiarow okna

--- Aplikacja/parkourSpots/src/app/components/faq-page/faq-page.component.ts
+++ Aplikacja/parkourSpots/src/app/components/faq-page/faq-page.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-faq-page',
   templateUrl: './faq-page.component.html',
   styleUrls: ['./faq-page.component.scss']
 })
-export class FaqPageComponent implements OnInit {
+export class FaqPageComponent implements OnInit, AfterViewChecked {
+
+  screenWidth: Number;
 
   q1: boolean;
   q2: boolean;
@@ -14,14 +17,26 @@ export class FaqPageComponent implements OnInit {
   q5: boolean;
 
   constructor() {
+    this.onResize();
+
     this.q1 = false;
     this.q2 = false;
     this.q3 = false;
     this.q4 = false;
     this.q5 = false;
-   }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?) {
+    this.screenWidth = window.innerWidth;
+  }
 
   ngOnInit() {
+  }
+
+  ngAfterViewChecked() {
+    console.log(this.screenWidth);
+    this.onResize();
   }
 
 }
